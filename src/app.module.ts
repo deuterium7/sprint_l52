ccca609f648948b2ca18faceb3d4bbafe53619c4
Alex Zabornyi <post0778@gmail.com>
2023-10-02 16:28:27 +0300

    - init

---
+++ src/app.module.ts
@@ -0,0 +1,42 @@
+import * as process from 'process';
+import { Module } from '@nestjs/common';
+import { DataSource } from 'typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { CommentsModule } from './comments/comments.module';
+import { TasksModule } from './tasks/tasks.module';
+import { User } from './users/entities/user.entity';
+import { Comment } from './comments/entities/comment.entity';
+import { Task } from './tasks/entities/task.entity';
+import { AuthModule } from './auth/auth.module';
+import { UploadModule } from './upload/upload.module';
+import {Upload} from "./upload/entities/upload.entity";
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT ?? '3306'),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [User, Comment, Task, Upload],
+      synchronize: true,
+    }),
+    UsersModule,
+    CommentsModule,
+    TasksModule,
+    AuthModule,
+    UploadModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
